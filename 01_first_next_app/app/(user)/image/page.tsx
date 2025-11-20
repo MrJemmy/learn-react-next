c29b57fc32bc6a8a0f65c50306a6a57d3c19bbcc
mrjemmy <jbpatel1411@gmail.com>
2025-11-20 15:31:29 +0530

    image done

--- 01_first_next_app/app/(user)/image/page.tsx
+++ 01_first_next_app/app/(user)/image/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { lazy } from "react";
+import videoImage from "@/public/video.jpg";
 
 // public folder images can be accessed directly using slash(/) prefix
 
@@ -33,30 +35,51 @@ export default function ImageComponent() {
             alt="video"
             height={200}
             width={200}
-            className="w-full h-full object-cover rounded-md"
+            className="w-full h-full rounded-md"
             quality={50}
+            style={{
+              objectFit: "cover",
+            }}
           />
         </div>
 
         <div className="part1 m-2.5 h-[200px] w-[200px] relative">
           {/* when we use fill do not need to use height and width but parent need's to use relative */}
-          {/* objectFit attribute only works with fill */}
-          <Image src="/video.jpg" alt="video" fill={true} objectFit="cover" />
-        </div>
-
-        <div className="part1 m-2.5 h-[200px] w-[200px] relative">
-          {/* priority */}
           <Image
             src="/video.jpg"
             alt="video"
             fill={true}
-            objectFit="cover"
-            priority={false}
+            style={{
+              objectFit: "cover",
+            }}
           />
         </div>
 
         <div className="part1 m-2.5 h-[200px] w-[200px] relative">
-          {/* loader: not done */}
+          {/* priority: Next.js 16, the priority property has been deprecated in favor of the `preload` property */}
+          {/* use preload when we have single image that is large and want image to be load in head befor it load */}
+          {/* loading and preload can not be used at the same time so remove's default lazy loading  */}
+          <Image
+            src="/video.jpg"
+            alt="video"
+            fill={true}
+            preload={true} // false: Does not preload the image.  true:  Preloads the image by inserting a <link> in the <head>
+            className="object-cover"
+          />
+        </div>
+
+        <div className="part1 m-2.5 h-[200px] w-[200px] relative">
+          {/* placeholder: is blur then we need to use blueDataURL and also image must be imported then only effect works well*/}
+          {/* it has 3ed value `data:image/...` do RND */}
+          <Image
+            src={videoImage}
+            alt="video"
+            fill={true}
+            quality={100}
+            placeholder="blur"
+            blurDataURL=""
+            className="object-cover"
+          />
         </div>
       </div>
     </div>
